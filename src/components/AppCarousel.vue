40822593bbcce398c30266e5f67baf10c098d500
christianarca27 <christian.arca.mail@gmail.com>
2023-04-13 16:18:58 +0200

    feat: completata prima sezione con carosello reviews (e inserita in un componente apposito)

---
+++ src/components/AppCarousel.vue
@@ -0,0 +1,214 @@
+<script>
+export default {
+    name: "AppCarousel",
+
+    data() {
+        return {
+            reviews: [
+                {
+                    title: "It's a choice of quality for people with special needs",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta modi officiis quasi unde eaque ut eum, nam nobis ducimus quibusdam tenetur harum adipisci, optio omnis ab, asperiores eligendi ipsum iure.",
+                    userImage: "artist-testimonial-avatar-01.jpg",
+                    username: "Florence Themes",
+                    userRole: "Multimedia Admin",
+                },
+                {
+                    title: "High level of efficiency and scientific teaching methods",
+                    text: "I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.",
+                    userImage: "artist-testimonial-avatar-02.jpg",
+                    username: "Mina Hollace",
+                    userRole: "Freelancer",
+                },
+                {
+                    title: "Professional team of specialist and passionate mentors at reach",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta modi officiis quasi unde eaque ut eum, nam nobis ducimus quibusdam tenetur harum adipisci, optio omnis ab, asperiores eligendi ipsum iure.",
+                    userImage: "artist-testimonial-avatar-03.jpg",
+                    username: "Madley Pondor",
+                    userRole: "IT Specialist",
+                },
+                {
+                    title: "Test review",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta modi officiis quasi unde eaque ut eum, nam nobis ducimus quibusdam tenetur harum adipisci, optio omnis ab, asperiores eligendi ipsum iure.",
+                    userImage: "artist-testimonial-avatar-04.jpg",
+                    username: "Ricky Brown",
+                    userRole: "Designer",
+                },
+            ],
+
+            activeIndex: 0,
+        }
+    },
+
+    computed: {
+        previousIndex() {
+            if (this.activeIndex > 0) {
+                return this.activeIndex - 1;
+            }
+            else {
+                return this.activeIndex + this.reviews.length - 1;
+            }
+        },
+
+        nextIndex() {
+            if (this.activeIndex < this.reviews.length - 1) {
+                return this.activeIndex + 1;
+            }
+            else {
+                return (this.activeIndex + 1) % this.reviews.length;
+            }
+        }
+    },
+}
+</script>
+
+<template>
+    <div id="reviews-list-wrapper">
+        <ul id="reviews-list">
+            <li class="review">
+                <div class="review-title">
+                    {{ reviews[previousIndex].title }}
+                </div>
+
+                <div class="review-text">
+                    {{ reviews[previousIndex].text }}
+                </div>
+
+                <div class="review-user">
+                    <div class="review-user-icon">
+                        <img :src="'/images/' + reviews[previousIndex].userImage" alt="User image">
+                    </div>
+
+                    <div class="review-user-details">
+                        <strong class="review-username">{{ reviews[previousIndex].username }}</strong>
+                        <div class="review-user-role">/ {{ reviews[previousIndex].userRole }}</div>
+                    </div>
+                </div>
+            </li>
+
+            <li class="review active">
+                <div class="review-title">
+                    {{ reviews[activeIndex].title }}
+                </div>
+
+                <div class="review-text">
+                    {{ reviews[activeIndex].text }}
+                </div>
+
+                <div class="review-user">
+                    <div class="review-user-icon">
+                        <img :src="'/images/' + reviews[activeIndex].userImage" alt="User image">
+                    </div>
+
+                    <div class="review-user-details">
+                        <strong class="review-username">{{ reviews[activeIndex].username }}</strong>
+                        <div class="review-user-role">/ {{ reviews[activeIndex].userRole }}</div>
+                    </div>
+                </div>
+            </li>
+
+            <li class="review">
+                <div class="review-title">
+                    {{ reviews[nextIndex].title }}
+                </div>
+
+                <div class="review-text">
+                    {{ reviews[nextIndex].text }}
+                </div>
+
+                <div class="review-user">
+                    <div class="review-user-icon">
+                        <img :src="'/images/' + reviews[nextIndex].userImage" alt="User image">
+                    </div>
+
+                    <div class="review-user-details">
+                        <strong class="review-username">{{ reviews[nextIndex].username }}</strong>
+                        <div class="review-user-role">/ {{ reviews[nextIndex].userRole }}</div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <div id="rotate-commands">
+            <i v-for="(review, index) in reviews" class="fa-solid fa-circle" :class="index == activeIndex ? 'active' : ''"
+                @click="activeIndex = index"></i>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+#reviews-list-wrapper {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    padding: 0 50px;
+
+    #reviews-list {
+        display: flex;
+        margin-bottom: 2rem;
+
+        flex-flow: row nowrap;
+        gap: 50px;
+        overflow: hidden;
+
+        .review {
+            background-color: white;
+            width: calc(100% / 3 - 50px / 3 * 2);
+            padding: 2rem;
+            flex-shrink: 0;
+            opacity: .5;
+
+            &>* {
+                margin-bottom: 1rem;
+            }
+
+            &.active {
+                opacity: 1;
+            }
+
+            .review-title {
+                font-weight: bold;
+                font-size: 1.2rem;
+            }
+
+            .review-text {}
+
+            .review-user {
+                display: flex;
+                align-items: center;
+                gap: 2rem;
+
+                .review-user-icon {
+                    img {
+                        width: 80px;
+                        height: 80px;
+                        border-radius: 40px;
+                    }
+                }
+            }
+        }
+    }
+
+    #rotate-commands {
+        margin-bottom: 2rem;
+
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        i {
+            font-size: .3rem;
+            opacity: .5;
+            cursor: pointer;
+
+            &:hover {
+                font-size: .5rem;
+            }
+
+            &.active {
+                font-size: .5rem;
+                opacity: 1;
+            }
+        }
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,203 +1,22 @@
 <script>
-import SecondaryButton from './SecondaryButton.vue';
+import ServicesSection from './ServicesSection.vue';
 
 export default {
     name: "AppMain",
-
     data() {
-        return {
-            services: [
-                {
-                    image: "artist-box-image-01.png",
-                    name: "Online Courses",
-                    description: "Online art coaching now offers you a very powerfull way to redesign your artist mind.",
-                },
-                {
-                    image: "artist-box-image-02.png",
-                    name: "One to One",
-                    description: "Getting the necessary clarity about the current state to help you improve your ability.",
-                },
-                {
-                    image: "artist-box-image-03.png",
-                    name: "Antywhere",
-                    description: "Access to valuable and portable program which allow you to setup and live anywhere you want.",
-                },
-                {
-                    image: "artist-box-image-04.png",
-                    name: "On Time",
-                    description: "Punctuality is our top priority because it's an essential criteria to assess a program quality.",
-                },
-            ],
-        }
+        return {};
     },
 
     components: {
-        SecondaryButton,
+        ServicesSection
     },
 }
 </script>
 
 <template>
     <main>
-        <section id="section-services">
-            <h2>Artist Coaching</h2>
-
-            <strong>I understand what it takes to create.<br> I can help you with</strong>
-
-            <div class="background-white">
-                <div id="services-list" class="container-small">
-                    <ul>
-                        <li v-for="service in services">
-                            <img :src="'/images/' + service.image" alt="Sevice logo">
-                            <h3>{{ service.name }}</h3>
-                            <p>{{ service.description }}</p>
-                        </li>
-                    </ul>
-                </div>
-
-                <div id="goal" class="container-small">
-                    <div id="goal-left">
-                        <img src="/images/artist-quote-icon.png" alt="">
-                        <strong>I coach and mentor visual artists, like you, to Create Like You Mean It</strong>
-                        <span id="sign">Martin Garrix</span>
-                    </div>
-                    <div id="goal-right">
-                        <p>As a coach and mentor, my ultimate goal is to motivate visual artists toward inspirated action. I
-                            help you get you back on track to being the productive, creative, meaningful and authentic
-                            artist
-                            you truly want to be. I support artist in crafting and living a creative life as well as feeling
-                            good about themselves and their art.</p>
-                        <p>As an artist and an educator for nearly 30 years, I understand what it takes to create.</p>
-                        <SecondaryButton :text="'Get started today'"></SecondaryButton>
-                    </div>
-                </div>
-
-                <div id="video-post" class="container-small">
-                    <img id="video-post-shape-1" src="/images/maxcoach-shape-05-150x150.png" alt="">
-                    <img id="video-post-shape-2" src="/images/maxcoach-shape-12-150x150.png" alt="">
-                    <img id="btn-play-youtube" src="/images/icon-youtube-play.png" alt="Youtube play button">
-                    <img id="video-post-cover" src="/images/artist-video-poster.jpg" alt="Poster video Youtube">
-                </div>
-            </div>
-        </section>
-
-        <section>
-        </section>
+        <ServicesSection></ServicesSection>
     </main>
 </template>
 
-<style lang="scss" scoped>
-#section-services {
-    padding: 50px 0;
-
-    background-image: url("/images/maxcoach-shape-14.png");
-    background-size: contain;
-    background-position: top;
-    background-repeat: no-repeat;
-
-
-    h2 {
-        text-align: center;
-        font-size: 2rem;
-    }
-
-    strong {
-        display: block;
-        margin-bottom: 3rem;
-        text-align: center;
-        font-size: 1.7rem;
-    }
-
-    #services-list {
-        ul {
-            display: flex;
-            gap: 2rem;
-
-            li {
-                img {
-                    height: 100px;
-                    width: auto;
-                }
-            }
-
-            li:nth-child(even) {
-                position: relative;
-                top: 30px;
-            }
-        }
-    }
-
-    #goal {
-        padding: 150px 0;
-
-        display: flex;
-        justify-content: space-between;
-
-        #goal-left {
-            width: 40%;
-
-            display: flex;
-            flex-flow: column nowrap;
-            align-items: center;
-            gap: 1rem;
-
-            text-align: center;
-
-            strong {
-                font-size: 2.2rem;
-            }
-
-            #sign {
-                font-family: 'Caveat', cursive;
-                color: #6ac5f1;
-                font-size: 2rem;
-            }
-        }
-
-        #goal-right {
-            width: 50%;
-            padding-top: calc(32px + 1rem);
-
-            display: flex;
-            flex-flow: column nowrap;
-            align-items: start;
-            gap: 2rem;
-        }
-    }
-
-    #video-post {
-        width: 50%;
-
-        display: flex;
-        justify-content: center;
-
-        position: relative;
-
-        #video-post-shape-1 {
-            position: absolute;
-            top: -30px;
-            left: -30px;
-        }
-
-        #video-post-shape-2 {
-            position: absolute;
-            bottom: -10px;
-            right: -100px;
-        }
-
-        #btn-play-youtube {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            z-index: 2;
-            cursor: pointer;
-        }
-
-        #video-post-cover {
-            width: 100%;
-            z-index: 1;
-        }
-    }
-}
-</style>
+<style lang="scss" scoped></style>
